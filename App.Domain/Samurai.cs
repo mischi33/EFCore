7cf4850adf3f68c37ee387726c373887a3c8b8a7
michelle.weber <michelle.weber@fusonic.net>
2021-11-26 15:00:42 +0100

    Add Domain Classes

---
+++ App.Domain/Samurai.cs
@@ -0,0 +1,9 @@
+namespace App.Domain
+{
+    public class Samurai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; } = new List<Quote>();
+    }
+}
